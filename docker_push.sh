a67574f2dee3ab139bf7ff07916d212c97989729
Akhilesh Boodhun <quantum19992011@gmail.com>
2021-07-18 19:35:46 +0400

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push ernestgwilsonii/docker-raspberry-pi-mumble-server:$MUMBLE_SERVER_VERSION
-docker tag ernestgwilsonii/docker-raspberry-pi-mumble-server:$MUMBLE_SERVER_VERSION ernestgwilsonii/docker-raspberry-pi-mumble-server:latest
-docker push ernestgwilsonii/docker-raspberry-pi-mumble-server:latest
+docker push akhileshboodhun/docker-raspberry-pi-mumble-server:$MUMBLE_SERVER_VERSION
+docker tag akhileshboodhun/docker-raspberry-pi-mumble-server:$MUMBLE_SERVER_VERSION akhileshboodhun/docker-raspberry-pi-mumble-server:latest
+docker push akhileshboodhun/docker-raspberry-pi-mumble-server:latest
